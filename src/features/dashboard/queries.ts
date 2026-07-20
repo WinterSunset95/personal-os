d746ca22a60a8756a7792f9ee3f173e71bd2575d
WinterSunst95 <wintersunset95@gmail.com>
2026-07-21 02:48:33 +0530

    refactor: removed redundant files

--- src/features/dashboard/queries.ts
+++
@@ -1,6 +0,0 @@
-import { TaskService } from "@/services/task.service";
-import type { TaskQuery } from "@/features/tasks/task-query";
-
-export async function getDashboardData(query?: TaskQuery) {
-  return TaskService.getDashboardData(query);
-}
--- src/features/tasks/search-actions.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { TaskService } from "@/services/task.service";
-
-export async function searchTasks(term: string) {
-  return TaskService.searchTasks(term);
-}
